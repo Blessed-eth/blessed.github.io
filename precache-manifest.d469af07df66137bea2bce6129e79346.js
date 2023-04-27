3f15c38bf1795059206ffea247592aaffdbd63d3
Joaquin <jfarfantorres@gmail.com>
2023-04-26 23:24:05 -0400

    Updates

---
+++ precache-manifest.d469af07df66137bea2bce6129e79346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc8cdb1953b5ba5a5374fc7659628bd",
+    "url": "/blessed.github.io/index.html"
+  },
+  {
+    "revision": "3034b7d94a098c7ff3f6",
+    "url": "/blessed.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5dff105d4aabff15a611",
+    "url": "/blessed.github.io/static/css/main.c9ecec30.chunk.css"
+  },
+  {
+    "revision": "3034b7d94a098c7ff3f6",
+    "url": "/blessed.github.io/static/js/2.ee8de792.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/blessed.github.io/static/js/2.ee8de792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dff105d4aabff15a611",
+    "url": "/blessed.github.io/static/js/main.e94fefb4.chunk.js"
+  },
+  {
+    "revision": "7c4b4f6857c4bb8100aa",
+    "url": "/blessed.github.io/static/js/runtime-main.baca385b.js"
+  }
+]);
