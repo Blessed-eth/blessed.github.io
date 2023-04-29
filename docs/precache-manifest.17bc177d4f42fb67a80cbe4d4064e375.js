228677553da3db46680dec5e1df1123b31630826
Joaquin <jfarfantorres@gmail.com>
2023-04-29 13:46:06 -0400

    renamed build dir

---
+++ docs/precache-manifest.17bc177d4f42fb67a80cbe4d4064e375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b4fe57617085bc2b75565d699567f0",
+    "url": "/blessed.github.io/index.html"
+  },
+  {
+    "revision": "1125f2890d601de6dec0",
+    "url": "/blessed.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9c53451d92a600f2c2d7",
+    "url": "/blessed.github.io/static/css/main.60be9013.chunk.css"
+  },
+  {
+    "revision": "1125f2890d601de6dec0",
+    "url": "/blessed.github.io/static/js/2.8a5a2417.chunk.js"
+  },
+  {
+    "revision": "d9595ca7a8f41e2d56f082fe5aa19556",
+    "url": "/blessed.github.io/static/js/2.8a5a2417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c53451d92a600f2c2d7",
+    "url": "/blessed.github.io/static/js/main.42327768.chunk.js"
+  },
+  {
+    "revision": "7c4b4f6857c4bb8100aa",
+    "url": "/blessed.github.io/static/js/runtime-main.baca385b.js"
+  }
+]);
